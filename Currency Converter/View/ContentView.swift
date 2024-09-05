8e24ec6d4af11f0e33c290a7332d1e2ace395060
Danil Anikin <mart4r.clearlist@gmail.com>
2024-09-05 19:31:44 +1000

    Init commit

---
+++ Currency Converter/View/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  Currency Converter
+//
+//  Created by anikin02 on 05.09.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        CurrencyConverterView()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Currency Converter/View/CurrencyConverterView.swift
@@ -0,0 +1,14 @@
+//
+//  CurrencyConverterView.swift
+//  Currency Converter
+//
+//  Created by anikin02 on 05.09.2024.
+//
+
+import SwiftUI
+
+struct CurrencyConverterView: View {
+  var body: some View {
+    /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Hello, world!@*/Text("Hello, world!")/*@END_MENU_TOKEN@*/
+  }
+}
